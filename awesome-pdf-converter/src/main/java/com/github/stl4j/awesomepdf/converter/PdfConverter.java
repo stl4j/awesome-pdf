0cc63d60a60324e6a0cdd450bd06bc32d44677de
stl4j <56674109+codeboyzhou@users.noreply.github.com>
2022-08-29 01:57:32 +0800

    AWESOME-PDF(#1): Add java docs

--- awesome-pdf-converter/src/main/java/com/github/stl4j/awesomepdf/converter/PdfConverter.java
+++ awesome-pdf-converter/src/main/java/com/github/stl4j/awesomepdf/converter/PdfConverter.java
@@ -5,16 +5,44 @@ import com.github.stl4j.awesomepdf.converter.support.ExcelConverter;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
-public abstract class PdfConverter {
+/**
+ * This class is an utility class, which provides some static methods
+ * to make it easier to use this library, you don't need to instantiate it.
+ * <p>
+ * Just use like this:
+ * <blockquote><pre>
+ *     PdfConverter.fromExcel("test.xslx").saveAsPdf();
+ *     PdfConverter.fromExcel("test.xslx").saveAsPdf("test.pdf");
+ * </pre></blockquote><p>
+ *
+ * @author stl4j <im.zhouchen@foxmail.com>
+ * @see ExcelConverter
+ * @since 0.0.1
+ */
+@SuppressWarnings("unused")
+public final class PdfConverter {
 
+    private PdfConverter() {
+        // An utility class should not be instantiated directly.
+    }
+
+    /**
+     * @see ExcelConverter(String)
+     */
     public static ExcelConverter fromExcel(String sourceFilePath) throws Exception {
         return new ExcelConverter(sourceFilePath);
     }
 
+    /**
+     * @see ExcelConverter(InputStream)
+     */
     public static ExcelConverter fromExcel(InputStream inputStream) throws Exception {
         return new ExcelConverter(inputStream);
     }
 
+    /**
+     * @see ExcelConverter(ByteArrayOutputStream)
+     */
     public static ExcelConverter fromExcel(ByteArrayOutputStream outputStream) throws Exception {
         return new ExcelConverter(outputStream);
     }
